f50deee949a5525ebe4d6e83e4d749fe78a3720b
linujern <linus@braincurrent.se>
2026-01-23 14:16:32 +0100

    car data population and car data display

--- RaceGame/Assets/Scripts/Player/CarSelect.cs
+++ RaceGame/Assets/Scripts/Player/CarSelect.cs
@@ -1,18 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 
 public class CarSelect : MonoBehaviour
 {
     [SerializeField] private GameObject[] cars;
     [SerializeField] private int selectedCar = 0;
     [SerializeField] private string sceneToLoad;
-  
+    
+    [SerializeField] private TMPro.TMP_Text AccelerationValueText;
+    [SerializeField] private TMPro.TMP_Text SpeedValueText;
+    [SerializeField] private TMPro.TMP_Text TurnValueText;
+    [SerializeField] private TMPro.TMP_Text WeightValueText;
+
+    private void Start()
+    {
+        CarDataUpdate();
+    }
+
     public void NextCar()
     {
         cars[selectedCar].SetActive(false);
         selectedCar = (selectedCar + 1) % cars.Length;
         cars[selectedCar].SetActive(true);
+        cars[selectedCar].transform.position = new Vector3(0, .4f, 0);
     }
 
     public void PreviousCar()
@@ -22,11 +33,23 @@ public class CarSelect : MonoBehaviour
         if(selectedCar < 0)
             selectedCar += cars.Length;
         cars[selectedCar].SetActive(true);
+        cars[selectedCar].transform.position = new Vector3(0, .4f, 0);
     }
 
     public void StartGame() 
     {
         PlayerPrefs.SetInt("selectedCar", selectedCar);
         SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+
+    public void CarDataUpdate()
+    {
+        Movement movement = cars[selectedCar].GetComponent<Movement>();
+        Rigidbody rb = cars[selectedCar].GetComponent<Rigidbody>();
+        
+        AccelerationValueText.SetText(movement.maxAcceleration.ToString("F0"));
+        SpeedValueText.SetText(movement.maxSpeed.ToString("F0"));
+        TurnValueText.SetText((movement.turnSensitivity * 100).ToString("F0"));
+        WeightValueText.SetText(rb.mass.ToString("F0"));
     }
 }
--- RaceGame/Assets/Scripts/Player/Movement.cs
+++ RaceGame/Assets/Scripts/Player/Movement.cs
@@ -20,6 +20,7 @@ public class Movement : MonoBehaviour
     }
 
     public float maxAcceleration = 30.0f;
+    public float maxSpeed = 300.0f;
     public float brakeAcceleration = 50.0f;
 
     public float turnSensitivity = 1.0f;
@@ -39,6 +40,7 @@ public class Movement : MonoBehaviour
     {
         carRb = GetComponent<Rigidbody>();
         carRb.centerOfMass = _centerOfMass;
+        carRb.maxLinearVelocity = maxSpeed;
     }
 
 
@@ -75,14 +77,14 @@ public class Movement : MonoBehaviour
         }
     }
 
-    public void Steer(float movement)
+    public void Steer(float steering)
     {
         foreach (var wheel in wheels)
         {
             if (wheel.axel == Axel.Front)
             {
                 float targetAngle =
-                    movement * turnSensitivity * maxSteerAngle;
+                    steering * turnSensitivity * maxSteerAngle;
 
                 wheel.wheelCollider.steerAngle =
                     Mathf.Lerp(
